845907a1f32b241c41127858a09a1f6e8408ad6d
IndieCoderMM <hthant00chk@gmail.com>
2024-06-27 20:30:30 +0630

    Add keys and colors

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  "uloco/bluloco.nvim",
+  lazy = false,
+  priority = 1000,
+  dependencies = { "rktjmp/lush.nvim" },
+}
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+--  {
+--    "sainnhe/sonokai",
+--  priority = 1000,
+--  config = function()
+--    vim.g.sonokai_transparent_background = "1"
+--    vim.g.sonokai_enable_italic = "1"
+--    vim.g.sonokai_style = "andromeda"
+--    vim.cmd.colorscheme("sonokai")
+--    end,
+--    },
+--  }
